df4a56713c3193cd8546ad7dd5d417d119e41739
Oleksii Bespalyi <skripkinlexxx@gmail.com>
2024-05-05 17:13:03 +0200

    add index file

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,8 @@
 const sortByDescendingFriendCount = (users) => {
-  return users.sort((a, b) => b.friends.length - a.friends.length);
+  return users
+    .slice() // створюємо копію масиву, щоб уникнути зміни вихідного масиву
+    .sort((a, b) => b.friends.length - a.friends.length)
+    .toSorted(); // додано метод toSorted()
 };
 
 console.log(
